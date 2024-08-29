f1d3e07090da2323aa18fc32cf648d6bc4877d49
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-08-29 07:29:00 -0500

    Initial config for personal portfolio

---
+++ src/components/Test.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<div>
+    <h1>Hello World</h1>
+    <p>From Svelte!</p>
+</div>
+
+<style lang="scss">
+    div {
+        h1 {
+            color: blue;
+        }
+
+        p {
+            color: red;
+        }
+    }
+</style>
+
